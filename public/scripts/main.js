6e8feed007e0b2bd11682c456c167ebd5e4da3d7
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-07-03 15:30:04 +0900

    Make a prototype chat

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,22 @@
+window.onload = function() {
+
 // データベースの参照を準備
-var firebaseRef = new Firebase("https://chattool-5a67b.firebaseio.com/"); // ... 1
-var messagesRef = firebaseRef.child('messages'); // ... 2
+  var firebaseRef = new Firebase("https://chattool-5a67b.firebaseio.com/"); // ... 1
+  var messagesRef = firebaseRef.child('messages'); // ... 2
 
 // 既存メッセージを表示
-messagesRef.on('child_added', function(snapshot) { // ... 3
-  var msg = snapshot.val();
-  $('<li>').text(msg.name + ': ' + msg.body).prependTo('#messages');
-});
-
-$('#send').click(function() {
-  // 新規メッセージを投稿
-  messagesRef.push({ // ... 4
-    name: $('#name').val(),
-    body: $('#message').val()
+  messagesRef.on('child_added', function(snapshot) { // ... 3
+    var msg = snapshot.val();
+    $('<li>').text(msg.name + ': ' + msg.body).prependTo('#messages');
   });
-});
+
+  $('#sendMessage').click(function() {
+    // 新規メッセージを投稿
+    messagesRef.push({ // ... 4
+      name: 'test',
+      body: $('#message').val()
+    });
+  });
+};
+
+
