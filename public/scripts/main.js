79762a5e5a1057b967f65748bbf441a961f478e5
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-07-04 00:14:43 +0900

    Update auth

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,8 @@
 class ChatClass{
   constructor() {
     // データベースの参照を準備
-    var firebaseRef = new Firebase("https://chattool-5a67b.firebaseio.com/");
-    this.messagesRef = firebaseRef.child('messages');
+    this.firebaseRef = new Firebase("https://chattool-5a67b.firebaseio.com/");
+    this.messagesRef = this.firebaseRef.child('messages');
 
     this.send_message = document.getElementById('sendMessage');
     this.login_button = document.getElementById('loginButton');
@@ -11,7 +11,7 @@ class ChatClass{
     this.send_message.addEventListener('click', this.saveMessage.bind(this));
     this.login_button.addEventListener('click', this.loginByGoogle.bind(this));
 
-    this.messagesRef.on('child_added', this.loadMessages.bind(this));
+    this.loadMessages();
 
     this.initFirebase();
   }
@@ -20,32 +20,60 @@ class ChatClass{
     this.auth = firebase.auth();
     this.database = firebase.database();
     this.storage = firebase.storage();
+    this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+  }
+
+  //  ログイン、ログアウト時の処理
+  onAuthStateChanged(user) {
+    if(user) {
+      //  ログイン時
+      let userName = user.displayName;
+      // this.loadMessages();
+    } else {
+      //ログアウト時
+    }
   }
 
   //  googleアカウントでログイン
   loginByGoogle(googleUser) {
+    $('#loginModal').modal('hide');
     let provider = new firebase.auth.GoogleAuthProvider();
-    this.auth.signInWithPopup(provider);
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   }
 
+  //  全メッセージの読み込みと、新規メッセージの読み込み
+  loadMessages() {
+    var setMessage = function(data) {
+      var msg = data.val();
+      $('<li>').text(msg.name + ': ' + msg.body).prependTo('#messages');
+    }.bind(this);
 
-  //  メッセージを表示
-  loadMessages(data) {
-    var msg = data.val();
-    $('<li>').text(msg.name + ': ' + msg.body).prependTo('#messages');
+     this.messagesRef.limitToLast(12).on('child_added', setMessage);
   }
 
   //  メッセージを投稿
   saveMessage() {
+    let currentUser = this.auth.currentUser;
     this.messagesRef.push({
-      name: 'test',
+      name: currentUser.displayName,
       body: $('#message').val()
     });
   }
-
-
-  //  ログイン認証
-  
 }
 
 window.onload = function() {
