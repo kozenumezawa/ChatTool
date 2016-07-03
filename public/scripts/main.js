08b4fb48b86a0a9688dcc90fabf16a3280ff3759
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-07-03 21:11:22 +0900

    Add mordal dialog

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,11 +5,29 @@ class ChatClass{
     this.messagesRef = firebaseRef.child('messages');
 
     this.send_message = document.getElementById('sendMessage');
+    this.login_button = document.getElementById('loginButton');
+
+
     this.send_message.addEventListener('click', this.saveMessage.bind(this));
+    this.login_button.addEventListener('click', this.loginByGoogle.bind(this));
 
     this.messagesRef.on('child_added', this.loadMessages.bind(this));
 
+    this.initFirebase();
   }
+
+  initFirebase() {
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+  }
+
+  //  googleアカウントでログイン
+  loginByGoogle(googleUser) {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    this.auth.signInWithPopup(provider);
+  }
+
 
   //  メッセージを表示
   loadMessages(data) {
@@ -17,13 +35,17 @@ class ChatClass{
     $('<li>').text(msg.name + ': ' + msg.body).prependTo('#messages');
   }
 
-  // メッセージを投稿
+  //  メッセージを投稿
   saveMessage() {
     this.messagesRef.push({
       name: 'test',
       body: $('#message').val()
     });
   }
+
+
+  //  ログイン認証
+  
 }
 
 window.onload = function() {
