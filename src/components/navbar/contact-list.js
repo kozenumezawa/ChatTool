792957d5b12ebb1f29c81365186088bd59946077
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-07-10 21:00:10 +0900

    Update contact-list

--- src/components/navbar/contact-list.js
+++ src/components/navbar/contact-list.js
@@ -6,14 +6,33 @@ import { NavItem } from 'react-bootstrap'
 export default class ContactList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.render_contact = [];
+  }
+
+  //  検索によって得たユーザーデータに合わせて、表示するテーブルを変える
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.parent_state.contact_list != this.props.parent_state.contact_list){
+      this.render_contact = [];
+
+      nextProps.parent_state.contact_list.forEach((element, index) => {
+        this.render_contact.push(
+          <NavItem eventKey={ index } href="#">
+            { element.friend_name }
+          </NavItem>
+        );
+      });
+    }
   }
 
   render() {
+    const active_key = 0;
     return (
-      <Nav bsStyle="pills" activeKey={1} stacked>
-        <NavItem eventKey={1} href="#">Home</NavItem>
-        <NavItem eventKey={2} href="#">About</NavItem>
-        <NavItem eventKey={3} href="#">Contact</NavItem>
+      <Nav bsStyle="pills" activeKey={ active_key } stacked>
+        <NavItem eventKey={ active_key } href="#">
+          連絡先リスト
+        </NavItem>
+        { this.render_contact }
       </Nav>
     );
   }
