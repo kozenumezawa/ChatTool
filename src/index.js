893a3a5fcf9c4d8b768de9f38eb5738fa6d07d1b
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2016-07-21 20:52:34 +0900

    Delete unused lines

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
 import Main from './components/main'
-// import mynavbar from './components/navbar/mynavbar'
-
 
 ReactDOM.render(
   <Main />,
